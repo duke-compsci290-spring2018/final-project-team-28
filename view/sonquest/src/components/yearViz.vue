48c2c2e1af9da44a21c84d8a71220c0754a2523e
Asa Royal <ajr74@duke.edu>
2018-04-23 15:58:27 -0400

    vue skeleto

---
+++ view/sonquest/src/components/yearViz.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="yearViz">
+    <h1>yearViz</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'yearViz',
+  data () {
+    return {
+      msg: 'yearViz',
+      curWeek: 1,
+      curYear: 1965 ,
+
+    }
+  },
+
+  computed: {
+
+     
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

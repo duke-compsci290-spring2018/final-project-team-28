45b38437ef0f1a95f787883fad97e4b3544eb900
Asa Royal <ajr74@duke.edu>
2018-04-24 09:52:30 -0400

    new vue components, firebase addded

---
+++ billscrape.py
@@ -0,0 +1,28 @@
+from datetime import date, timedelta, datetime
+import billboard
+import pyrebase 
+import credentials
+config = credentials.login
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password('liljinx@runeterra.com', 'caitlyn')
+db = firebase.database()
+
+
+start = 1-2-1965
+start_date= datetime.strptime('1965-01-01', '%Y-%m-%d')
+
+for i in range(1000):
+    date_str = start_date.strftime('%Y-%m-%d')
+    real_date = (start_date + timedelta(days=1)).strftime('%Y-%m-%d')
+    cur_year = (start_date + timedelta(days=1)).year
+    print(date_str)
+    chart = billboard.ChartData(name='hot-100', date=date_str)
+    #r1  = db.child('weeks').child(real_date)
+    for num,song in enumerate(chart):
+        entry = {'week': real_date, 'rank': num, 'track': song.title, 'artist': song.artist}
+        results = db.child(cur_year).child(real_date).child(num+1).set(entry, user['idToken'])
+        print(entry)
+        if num == 4:
+            break
+    start_date = start_date + timedelta(days=7)
--- view/sonquest/src/components/yearViz.vue
+++ view/sonquest/src/components/yearViz.vue
@@ -1,24 +1,60 @@
 <template>
   <div class="yearViz">
     <h1>yearViz</h1>
+    {{songRanks}}
   </div>
 </template>
 
 <script>
+var billboard = require('billboard-top-100').getChart;
+import Vue from 'vue'
+var VueFire = require('vuefire')
+var firebase = require('firebase')
+var config = {
+  apiKey: "AIzaSyDSRaDpwgpKA6QPsGLlpi4jc5F0t2Cglz0",
+  authDomain: "sonquest-3379c.firebaseapp.com",
+  databaseURL: "https://sonquest-3379c.firebaseio.com",
+  projectId: "sonquest-3379c",
+  storageBucket: "sonquest-3379c.appspot.com",
+  messagingSenderId: "228676657274"
+};
+// global access to initialized app database
+var db = firebase.initializeApp(config).database();
+var storageRef = firebase.storage().ref();
+// global reference to remote data
+var yearRef = db.ref('1965');
+// connect Firebase to Vue
+Vue.use(VueFire);
 export default {
   name: 'yearViz',
   data () {
     return {
       msg: 'yearViz',
       curWeek: 1,
-      curYear: 1965 ,
-
+      curYear: 1965
     }
   },
 
-  computed: {
+  firebase: {
+    weeks: yearRef
+  },
 
-     
+  computed: {
+    songRanks: function () {
+      var songs = [];
+      this.weeks.forEach(function(elem){
+        songs.push(elem);
+      });
+      return songs;
+    },
+    allSongs: function () {
+      var songs = new Set();
+      this.weeks.forEach(function(elem){
+        elem['.value'].forEach(function(element){
+          songs.add(element.track);
+        });
+      });
+    }
   }
 }
 </script>

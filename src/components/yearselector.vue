f9123c24b31f9f83d20aac3db95c0653a7dc72c0
Gordon <ghh6@duke.edu>
2018-04-25 16:52:40 -0400

    gitpages stuff

---
+++ src/components/yearselector.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="years">
+    <span v-for="yr in yearList">
+      <router-link :to='"/"+yr'>{{yr}} </router-link> 
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'yearselector',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  computed: {
+    yearList: function () {
+      var year = (new Date ()).getFullYear();
+      var yearRange = [];
+      for(var i = 1965; i <= year; i++){
+        yearRange.push(i);
+      }
+      return yearRange; 
+
+    } 
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
